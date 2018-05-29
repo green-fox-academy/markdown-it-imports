9dfbb31f6117f91b4ec915f7e2737375716f1437
bbosze <b.bosze@gmail.com>
2018-05-29 13:59:36 +0200

    seems to be ok

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-var md = require('markdown-it')();
-var Plugin = require('markdown-it-regexp');
+'use strict';
+
+const fs = require('fs');
+const md = require('markdown-it')();
+const Plugin = require('markdown-it-regexp');
+
+const mainFileString = fs.readFileSync('mainfile.md', 'utf8');
 
 var plugin = Plugin(
   // regexp to match
-  /^@import\(\w+\.md\)$/,
+  /^@import\(.+\.md\)$/,
   // this function will be called when something matches
-  function(match, utils) {
-    var url = 'http://example.org/u/' + match[0]
-
-    return '<a href="' + utils.escape(url) + '">'
-         + utils.escape(match[0])
-         + '</a>'
+  function(match) {
+    let importedMd = fs.readFileSync(match[0].slice(8, -1), 'utf8');
+    return importedMd
   }
 )
 
 md.use(plugin)
 
-var res = md.render("@import(ndsajdnasjdinasd.md)")
-
+var res = md.render(mainFileString)
 console.log(res);
---
+++ mainfile.md
@@ -0,0 +1,11 @@
+## Hello hallo
+
+@import(somefile.md)
+
+### Bello Hello
+
+@import(otherfile.md)
+
+THIS IS NOT A MATCH: @import(otherfile.mddsfdsfdsfdsf)
+THIS IS NOT A MATCH: @import(otherfile.mddsfdsfdsfdsf)
+THIS IS NOT A MATCH: @import(otherfile.mddsfdsfdsfdsf)
